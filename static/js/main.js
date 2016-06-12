e3986fe392bdf8b8794f733025a96fbcc7fb471c
Sanchit Goel <sanchit.goel1123@gmail.com>
2016-06-13 01:17:47 +0530

    Added numb TURN server for connectivity between different networks

--- static/js/main.js
+++ static/js/main.js
@@ -44,6 +44,10 @@ var ICE_config = {
         credential: 'muazkh',
         username: 'webrtc@live.com'
     }, {
+        url: 'turn:numb.viagenie.ca:3478',
+        username: 'sanchit.goel1123@gmail.com',
+        credential: 'qwerty'
+    }, {
         url: 'turn:192.158.29.39:3478?transport=udp',
         credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
         username: '28224511:1379330808'
